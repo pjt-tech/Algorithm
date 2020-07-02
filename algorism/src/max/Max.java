cf4b44db24e830ce5643ccd5d822ae54193b6094
pjt-tech <dktkd7044@gmail.com>
2020-07-02 22:25:25 +0900

    max

--- algorism/src/max/Max.java
+++ algorism/src/max/Max.java
@@ -7,6 +7,7 @@ public class Max {
 	public static void main(String[] args) {
 		
 		int a,b,c;
+		//최대값,최소값 저장 할 변수
 		int max,min;
 		
 		Scanner s = new Scanner(System.in);
@@ -19,15 +20,17 @@ public class Max {
 		
 		System.out.print("c값 : ");
 		c = s.nextInt();
-		
+		 
+		//조건문을 통하여 조건에 해당하면 최대값을 바꿔준다. 순차적 구조
 		max = a;
 		if(b>max) max = b;
 		if(c>max) max = c;
 		
+		//반대로 최소값을 구합니다.
 		min = b;
 		if(a<min) min = a;
 		if(c<min) min = c;
-		
+	
 		System.out.println("최대값 : " + max);
 		System.out.println("최소값 : " + min);
 		
